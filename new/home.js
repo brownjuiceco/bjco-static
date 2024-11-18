409abb0d581453655b915a6a111c4265e69d8dc9
Brown Juice Co <5776635+brimwd@users.noreply.github.com>
2024-11-18 20:05:36 +0900

    homepage updates

--- new/home.js
+++ new/home.js
@@ -3,4 +3,25 @@ $(document).ready(function() {
     console.log('click')
     $('#holistic-approach').toggleClass('animated')
   });
+
+  // set up animation
+  buildAnimation();
+  $(window).on('resize', buildAnimation);
 });
+
+function buildAnimation() {
+  const img = $('#ds-experts .anim-container .positioned.anim img');
+  const offset = .02;
+  const startingPos = img.height() * (.5 + offset) * -1;
+  const endingPos = img.height() * (0 + offset) * -1;
+
+  $('style[name="animation-container"]').remove();
+  $('body').append($(
+    `<style type="text/css" name="animation-container">
+      :root {
+        --startingPos: ${startingPos};
+        --endingPos: ${endingPos};
+      }
+    </style>`
+  ));
+}
